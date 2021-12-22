042d0123da7bc241ea3c37b601f955c23a5e3667
Won-young <dnjsdud2257@gmail.com>
2021-12-22 23:19:43 +0900

    메뉴 count 갯수 추가

--- srcForStudy/js/index.js
+++ srcForStudy/js/index.js
@@ -1,10 +1,10 @@
 // 1. 요구사항 정리
 // - 추가 -
 // [x] 에스프레소 메뉴에 새로운 메뉴를 확인버튼 클릭으로 추가한다.
-//     [ ] 엔터키 입렵으로 추가한다.
-// [ ] 메뉴 추가 후 input은 빈 값으로 초기화 한다.
-// [ ] 사용자 입력값이 빈값이면 추가하지 않는다.
-// [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
+//     [ x ] 엔터키 입렵으로 추가한다.
+// [x] 메뉴 추가 후 input은 빈 값으로 초기화 한다.
+// [x]] 사용자 입력값이 빈값이면 추가하지 않는다.
+// [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
 //     - 추가되는 메뉴의 아래 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 삽입해야 한다.
 // - 수정 -
 // [ ] 수정 버튼을 누르면 메뉴 이름을 입력할 수 있는 prompt를 띄운다.
@@ -17,7 +17,7 @@
 const $ = (tag) => document.querySelector(tag);
 
 function APP() {
-  const addMenu = (menuName) => {
+  const addMenuName = (menuName) => {
     const menuTemplate = `
       <li class="menu-list-item d-flex items-center py-2">
         <span class="w-100 pl-2 menu-name">${menuName}</span>
@@ -34,7 +34,14 @@ function APP() {
           삭제
         </button>
       </li>`;
+    if (menuName === "") {
+      alert("메뉴 이름을 입력해주세요");
+      return;
+    }
     $("#menu-list").insertAdjacentHTML("beforeend", menuTemplate);
+    const menuCount = $("#menu-list").querySelectorAll("li").length;
+    $(".menu-count").innerText = `총 ${menuCount}개`;
+    $("#menu-name").value = "";
   };
 
   $("#menu-form").addEventListener("keypress", (e) => {
@@ -43,15 +50,14 @@ function APP() {
   // 클릭해서 추가하기
   $("#menu-submit-button").addEventListener("click", () => {
     const menuName = $("#menu-name").value;
-    addMenu(menuName);
+    addMenuName(menuName);
   });
-  console.log("테스트");
 
   // 엔터키로 추가하기
   $("#menu-name").addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
       const menuName = $("#menu-name").value;
-      addMenu(menuName);
+      addMenuName(menuName);
     }
   });
 }

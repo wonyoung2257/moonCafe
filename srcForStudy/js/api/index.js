af576d192ad2cf6bb2e455aee5509298d677c450
Won-young <dnjsdud2257@gmail.com>
2021-12-27 16:20:50 +0900

    api 호출 객체 추상화

--- srcForStudy/js/api/index.js
+++ srcForStudy/js/api/index.js
@@ -1,58 +1,67 @@
 const BASE_URL = "http://localhost:3000";
 
-const MenuApi = {
-  async getAllMenuByCategory(category) {
-    const response = await fetch(`${BASE_URL}/api/category/${category}/menu`);
-    if (!response.ok) {
-      console.error("에러가 발생했습니다.");
-      alert("에러가 발생했습니다.");
-    }
-    return response.json();
-  },
-
-  async createMenu(category, name) {
-    const response = await fetch(`${BASE_URL}/api/category/${category}/menu`, {
+// 옵션을 객체로 만들어 재사용할 수 있게 함
+const HTTP_METHOD = {
+  POST(data) {
+    return {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ name }),
-    });
-    if (!response.ok) {
-      console.error("에러가 발생했습니다.");
-      alert("에러가 발생했습니다.");
-    }
+      body: JSON.stringify(data),
+    };
   },
-  async updateMenu(category, name, menuId) {
-    const response = await fetch(`${BASE_URL}/api/category/${category}/menu/${menuId}`, {
+  PUT(data) {
+    return {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ name }),
-    });
-    if (!response.ok) {
-      console.error("에러가 발생했습니다.");
-      alert("에러가 발생했습니다.");
-    }
+      body: data ? JSON.stringify(data) : null,
+    };
+  },
+  DELETE() {
+    return {
+      method: "DELETE",
+    };
+  },
+};
+
+const request = async (url, option) => {
+  const response = await fetch(url, option);
+  if (!response.ok) {
+    alert("에러가 발생했습니다.");
+    console.error(e);
+  }
+  return response.json();
+};
+
+const requestWithoutJson = async (url, option) => {
+  const response = await fetch(url, option);
+  if (!response.ok) {
+    alert("에러가 발생했습니다.");
+    console.error(e);
+  }
+  return response;
+};
+
+const MenuApi = {
+  async getAllMenuByCategory(category) {
+    return request(`${BASE_URL}/api/category/${category}/menu`); // 옵션에 아무것도 넣지않으면 GET으로 동작
+  },
+
+  async createMenu(category, name) {
+    return request(`${BASE_URL}/api/category/${category}/menu`, HTTP_METHOD.POST({ name }));
+  },
+  async updateMenu(category, name, menuId) {
+    return request(`${BASE_URL}/api/category/${category}/menu/${menuId}`, HTTP_METHOD.PUT({ name }));
   },
   async deleteMenu(category, menuId) {
-    const response = await fetch(`${BASE_URL}/api/category/${category}/menu/${menuId}`, {
-      method: "DELETE",
-    });
-    if (!response.ok) {
-      console.error("에러가 발생했습니다.");
-      alert("에러가 발생했습니다.");
-    }
+    //데이터가 없는 상태에서 데이터 파씽하여 에러발생
+    return requestWithoutJson(`${BASE_URL}/api/category/${category}/menu/${menuId}`, HTTP_METHOD.DELETE());
   },
   async toggleSoldOut(category, menuId) {
-    const response = await fetch(`${BASE_URL}/api/category/${category}/menu/${menuId}/soldout`, {
-      method: "PUT",
-    });
-    if (!response.ok) {
-      console.error("에러가 발생했습니다.");
-      alert("에러가 발생했습니다.");
-    }
+    return request(`${BASE_URL}/api/category/${category}/menu/${menuId}/soldout`, HTTP_METHOD.PUT());
   },
 };
 
